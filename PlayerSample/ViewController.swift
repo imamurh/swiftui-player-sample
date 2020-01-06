f9d1206ec4cde2b1728302ea34d67b6ba9458905
imamurh <imamurh@gmail.com>
2020-01-06 16:42:57 +0900

    Initial Commit

---
+++ PlayerSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlayerSample
+//
+//  Created by Hajime Imamura on 2020/01/06.
+//  Copyright © 2020 imamurh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
